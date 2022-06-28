4ef4cb5d33e4653443d26bc00e4e7cbc33cf3fed
Hunter Kassow <hunterkassow@protonmail.com>
2022-06-28 12:42:33 -0400

    all tests pass

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
-# Your code goes here!
+class Anagram 
+    attr_accessor :word, :letters 
+    def initialize word
+        @word = word
+        @letters = word.chars.sort
+        
+    end
+
+    def match array
+        result = []
+        array.each do |word|
+            if word.chars.sort == letters
+                result.push word
+            end
+        end
+        result
+    end
+
+end
+
+x = Anagram.new('listen')
+puts x.match((%w[enlists google inlets banana]))
